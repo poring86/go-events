cd78ec620309840cbc76dfcd925d360874740c19
poring86 <matheuslino86a@gmail.com>
2025-02-02 13:56:41 -0400

    producer

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -8,13 +8,16 @@ import (
 )
 
 func main() {
-	ch, err := rabbitmq.OppenChannel()
+	ch, err := rabbitmq.OpenChannel()
 	if err != nil {
 		panic(err)
 	}
 	defer ch.Close()
+
 	msgs := make(chan amqp.Delivery)
-	go rabbitmq.Consume(ch, msgs)
+
+	go rabbitmq.Consume(ch, msgs, "minhafila")
+
 	for msg := range msgs {
 		fmt.Println(string(msg.Body))
 		msg.Ack(false)
